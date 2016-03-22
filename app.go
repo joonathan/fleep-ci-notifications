2db282f5388a5231076018fb18c4c16f5337dd70
Joonathan Mägi <joonathan@teleport.org>
2016-03-22 13:17:30 +0200

    Support for BuildKite

--- app.go
+++ app.go
@@ -1,15 +1,19 @@
 package main
 
-import "os"
+import (
+	"encoding/json"
+	"os"
+	"regexp"
+)
 import "fmt"
 import "net/http"
 import "net/url"
-import "github.com/go-martini/martini"
-import "github.com/martini-contrib/binding"
 
-type CIData struct {
+import "github.com/go-martini/martini"
+
+type circleCIData struct {
 	Payload struct {
-		BuildUrl       string `json:"build_url"`
+		BuildURL       string `json:"build_url"`
 		BuildNum       int    `json:"build_num"`
 		Branch         string `json:"branch"`
 		RepositoryName string `json:"reponame"`
@@ -18,29 +22,141 @@ type CIData struct {
 	} `json:"payload"`
 }
 
-var template = `Build number *%d* based on a commit by *%s* to a *%s* repository branch *%s* completed on CircleCI with a status of *%s*.
+type buildKiteData struct {
+	Build struct {
+		ID      string      `json:"id"`
+		URL     string      `json:"url"`
+		WebURL  string      `json:"web_url"`
+		Number  int         `json:"number"`
+		State   string      `json:"state"`
+		Message string      `json:"message"`
+		Commit  string      `json:"commit"`
+		Branch  string      `json:"branch"`
+		Tag     interface{} `json:"tag"`
+		Source  string      `json:"source"`
+		Creator struct {
+			ID        string `json:"id"`
+			Name      string `json:"name"`
+			Email     string `json:"email"`
+			AvatarURL string `json:"avatar_url"`
+			CreatedAt string `json:"created_at"`
+		} `json:"creator"`
+		CreatedAt   string `json:"created_at"`
+		ScheduledAt string `json:"scheduled_at"`
+		StartedAt   string `json:"started_at"`
+		FinishedAt  string `json:"finished_at"`
+		MetaData    struct {
+			BuildkiteGitBranch string `json:"buildkite:git:branch"`
+			BuildkiteGitCommit string `json:"buildkite:git:commit"`
+		} `json:"meta_data"`
+	} `json:"build"`
+	Pipeline struct {
+		ID         string `json:"id"`
+		URL        string `json:"url"`
+		WebURL     string `json:"web_url"`
+		Name       string `json:"name"`
+		Slug       string `json:"slug"`
+		Repository string `json:"repository"`
+		Provider   struct {
+			ID       string `json:"id"`
+			Settings struct {
+				BuildPullRequests          bool   `json:"build_pull_requests"`
+				BuildTags                  bool   `json:"build_tags"`
+				PublishCommitStatus        bool   `json:"publish_commit_status"`
+				PublishCommitStatusPerStep bool   `json:"publish_commit_status_per_step"`
+				Repository                 string `json:"repository"`
+			} `json:"settings"`
+			WebhookURL string `json:"webhook_url"`
+		} `json:"provider"`
+		BuildsURL string `json:"builds_url"`
+		CreatedAt string `json:"created_at"`
+		Steps     []struct {
+			Type                string `json:"type"`
+			Name                string `json:"name"`
+			Command             string `json:"command"`
+			ArtifactPaths       string `json:"artifact_paths"`
+			BranchConfiguration string `json:"branch_configuration"`
+			Env                 struct {
+			} `json:"env"`
+			TimeoutInMinutes interface{}   `json:"timeout_in_minutes"`
+			AgentQueryRules  []interface{} `json:"agent_query_rules"`
+			Concurrency      interface{}   `json:"concurrency"`
+			Parallelism      interface{}   `json:"parallelism"`
+		} `json:"steps"`
+		Env struct {
+			NPMAUTHTOKEN string `json:"NPM_AUTH_TOKEN"`
+			TUTUMAUTH    string `json:"TUTUM_AUTH"`
+		} `json:"env"`
+		ScheduledBuildsCount int `json:"scheduled_builds_count"`
+		RunningBuildsCount   int `json:"running_builds_count"`
+		ScheduledJobsCount   int `json:"scheduled_jobs_count"`
+		RunningJobsCount     int `json:"running_jobs_count"`
+		WaitingJobsCount     int `json:"waiting_jobs_count"`
+	} `json:"pipeline"`
+	Sender struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"sender"`
+}
+
+var circleCITemplate = `Build number *%d* based on a commit by *%s* to a *%s* repository branch *%s* completed on CircleCI with a status of *%s*.
 More information is available on %s<<CircleCI>>.`
+
+var buildKiteTemplate = `Build number *%d* based on a commit by *%s* to a *%s* repository branch *%s* completed on BuildKite with a status of *%s*.
+More information is available on %s<<BuildKite>>.`
 
 func main() {
 	m := martini.Classic()
+
+	m.Get("/", func() (int, string) {
+		return 200, "I'm alive."
+	})
 
 	m.Post("/", func() (int, string) {
 		return 200, "I'm alive."
 	})
 
-	m.Post("/webhook/:auth/:hash", binding.Bind(CIData{}), func(params martini.Params, ci CIData) (int, string) {
+	m.Post("/webhook/:auth/:hash", func(params martini.Params, req *http.Request) (int, string) {
+		var message string
+
+		defer req.Body.Close()
+		j := json.NewDecoder(req.Body)
+
+		if req.UserAgent() == "Buildkite-Request" {
+			var buildkite buildKiteData
+			err := j.Decode(&buildkite)
+			if err != nil {
+				return 500, "Bad request."
+			}
+
+			r := regexp.MustCompile("Author:.{1,5}(?P<author>.+ <.+>)\\nA")
+			message = fmt.Sprintf(buildKiteTemplate,
+				buildkite.Build.Number,
+				r.FindStringSubmatch(buildkite.Build.MetaData.BuildkiteGitCommit)[1],
+				buildkite.Pipeline.Repository,
+				buildkite.Build.Branch,
+				buildkite.Build.State,
+				buildkite.Build.WebURL)
+		} else {
+			var circle circleCIData
+			err := j.Decode(&circle)
+			if err != nil {
+				return 500, "Bad request."
+			}
+
+			message = fmt.Sprintf(circleCITemplate,
+				circle.Payload.BuildNum,
+				circle.Payload.CommiterName,
+				circle.Payload.RepositoryName,
+				circle.Payload.Branch,
+				circle.Payload.Outcome,
+				circle.Payload.BuildURL)
+		}
+
 		if os.Getenv("WEBHOOK_SECRET") == params["auth"] {
-			fleep_url := "https://fleep.io/hook/" + params["hash"]
+			fleepURL := "https://fleep.io/hook/" + params["hash"]
 
-			message := fmt.Sprintf(template,
-				ci.Payload.BuildNum,
-				ci.Payload.CommiterName,
-				ci.Payload.RepositoryName,
-				ci.Payload.Branch,
-				ci.Payload.Outcome,
-				ci.Payload.BuildUrl)
-
-			resp, err := http.PostForm(fleep_url, url.Values{"message": {string(message)}})
+			resp, err := http.PostForm(fleepURL, url.Values{"message": {string(message)}})
 			if err != nil {
 				fmt.Println("Error during POST request to Fleep", err)
 				return 500, "Error calling Fleep Hook"
@@ -50,9 +166,9 @@ func main() {
 			fmt.Println("Fleep hook response status:", resp.Status)
 
 			return 200, "Successfully proxied hook " + params["hash"]
-		} else {
-			return 401, "Unauthorized."
 		}
+
+		return 401, "Unauthorized."
 	})
 
 	m.Run()
